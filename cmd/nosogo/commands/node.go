46519de17429ebb139dc123da4f03a8799f39705
Gustavo Carreno <guscarreno@gmail.com>
2024-08-02 17:06:12 +0100

    feat: Adding node command

---
+++ cmd/nosogo/commands/node.go
@@ -0,0 +1,101 @@
+package commands
+
+import (
+	"github.com/spf13/cobra"
+	//log "github.com/EIYARO-Project/core-stratumd/logger"
+	//"github.com/EIYARO-Project/core-stratumd/server"
+	//fs "github.com/EIYARO-Project/core-stratumd/utils"
+)
+
+// nodeCmd represents the node command
+var (
+	nodeCmd = &cobra.Command{
+		Use:   "node",
+		Short: "Starts the node",
+		//Long:  `Starts the web server.`,
+		Run: runNode,
+	}
+)
+
+func init() {
+	rootCmd.AddCommand(nodeCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// serveCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	/*serveCmd.Flags().StringVarP(&cfgFile, "config", "c", "", "config file (default is "+config.GetConfigFile()+")")
+
+	serveCmd.Flags().String(cAPIAddressFlag, config.API.Address, "API address")
+	viper.BindPFlag("api.address", serveCmd.Flags().Lookup(cAPIAddressFlag))
+
+	serveCmd.Flags().Int(cAPIPortFlag, config.API.Port, "API port")
+	viper.BindPFlag("api.port", serveCmd.Flags().Lookup(cAPIPortFlag))
+
+	serveCmd.Flags().String(cAPIAccessTokenFlag, config.API.AccessToken, "API access token")
+	viper.BindPFlag("api.access_token", serveCmd.Flags().Lookup(cAPIAccessTokenFlag))
+
+	serveCmd.Flags().String(cStratumAddressFlag, config.Stratum.Address, "Stratum address")
+	viper.BindPFlag("stratum.address", serveCmd.Flags().Lookup(cStratumAddressFlag))
+
+	serveCmd.Flags().Int(cStratumPortFlag, config.Stratum.Port, "Stratum port")
+	viper.BindPFlag("stratum.port", serveCmd.Flags().Lookup(cStratumPortFlag))
+
+	serveCmd.Flags().Int(cStratumMaxConnectionsFlag, config.Stratum.MaxConnections, "Stratum Max Connections")
+	viper.BindPFlag("stratum.max_connections", serveCmd.Flags().Lookup(cStratumMaxConnectionsFlag))*/
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// serveCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+func runNode(cmd *cobra.Command, args []string) {
+	/*log.Debug("serve called")
+	if fs.FileExists(viper.ConfigFileUsed()) {
+		log.Debugf("Stratum Address: '%s'", config.Stratum.Address)
+		log.Debugf("Stratum Port: %d", config.Stratum.Port)
+		log.Debugf("Stratum Max Connections: %d", config.Stratum.MaxConnections)
+		log.Debugf("Stratum Difficulty: %d", config.Stratum.Difficulty)
+		log.Debugf("API Address: '%s'", config.API.Address)
+		log.Debugf("API Port: %d", config.API.Port)
+		log.Debugf("API Access Token: '%s'", config.API.AccessToken)
+
+		// TODO: Make sure we have sane values for address and port
+
+		var waitGroup sync.WaitGroup
+
+		// Stratum Server
+		ctx := context.Background()
+		server := server.NewServer(
+			ctx, config.Stratum.Address,
+			config.Stratum.Port,
+			config.Stratum.MaxConnections,
+			config.Stratum.Difficulty,
+			config.API.Address,
+			config.API.Port,
+			config.API.AccessToken)
+
+		// Start Polling the node's API
+		waitGroup.Add(1)
+		go func() {
+			defer waitGroup.Done()
+			server.PollNode()
+		}()
+
+		// Start the server
+		waitGroup.Add(1)
+		go func() {
+			defer waitGroup.Done()
+			if err := server.ListenAndServe(); err != nil {
+				log.Fatalf("Error on ListenAndServe: %s", err)
+			}
+		}()
+
+		// Wait for all servers to exit
+		waitGroup.Wait()
+	} else {
+		log.Fatalf("Cannot find config file '%s', please run the 'init' command first", viper.ConfigFileUsed())
+	}*/
+}
