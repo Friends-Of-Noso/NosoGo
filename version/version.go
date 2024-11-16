ab76a080822114a8065a2d2e50daea7f6fd752d3
Gustavo Carreno <guscarreno@gmail.com>
2024-11-16 19:41:48 +0000

    chore: Bumping version for test of `CD`

--- version/version.go
+++ version/version.go
@@ -5,8 +5,8 @@ import "fmt"
 var (
 	VersionMajor = 0
 	VersionMinor = 0
-	VersionPatch = 1
-	Version      = "v0.0.1"
+	VersionPatch = 2
+	Version      = "v0.0.2"
 	Name         = "nosogo"
 	// GitCommit is set with --ldflags "-X main.gitCommit=$(git rev-parse HEAD)"
 	GitCommit string
